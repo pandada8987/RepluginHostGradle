822ea6cb9795133dfcabe0dda333dc31ac8d7afd
PANDA <742890757@qq.com>
2020-04-15 16:07:51 +0800

    first commit

---
+++ app/src/main/java/com/panda/replugin_host_gradle_horizontal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.panda.replugin_host_gradle_horizontal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
